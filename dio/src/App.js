c8209f88aeccc8bcb1ecdc67452c82ab3c90233a
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-08-10 20:31:27 -0300

    conceitos de componentes e Props

--- dio/src/App.js
+++ dio/src/App.js
@@ -1,9 +1,12 @@
 
-
+import Button from './components/button/index'
 function App() {
   return (
     <div className="App">
-      Olá React
+      <h1> Olá React </h1>
+
+      <Button title="Entrar" />
+
     </div>
   );
 }
