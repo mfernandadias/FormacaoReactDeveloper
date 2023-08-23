6a540ea145d7f4f695b18543384104812e506e86
Maria Fernanda Dias <mariadosantodias@gmail.com>
2023-08-23 13:00:54 -0300

    app.js

--- dio/src/App.js
+++ dio/src/App.js
@@ -1,5 +1,5 @@
 
-import Button from './components/button/index'
+//import Button from './components/button/index'
 
 const App = () => {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -15,4 +15,23 @@ const App = () => {
   );
 };
 
-export default App;
+export default App; 
+
+/*import React, { useState } from "react";
+
+class App = ( ) =>  {
+  const [usuarios, ] = useState(["Pablo", "José", "Manoel"])
+
+  render() {
+    const { usuarios } = this.state;
+    return (
+      <div className="App">
+        <h1>Hello DIO</h1>
+        {
+          usuarios.map((item) => (
+          <p>{item}</p>
+          ))}
+      </div>
+    );
+  }
+} */
